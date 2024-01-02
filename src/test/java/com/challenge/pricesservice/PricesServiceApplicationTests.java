26bca28c9343d1fbcd7505890a928672f4350675
carlosvalencia <cvalencia@inno-it.es>
2024-01-02 11:39:15 +0100

    Create base-line project

---
+++ src/test/java/com/challenge/pricesservice/PricesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.pricesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
